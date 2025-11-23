b8a93fe49d07df27215893333d5003ef184df06b
deepanshkhurana <deepanshkhurana@outlook.com>
2025-11-24 04:18:20 +0530

    feat(opengraph): add opengraph meta tags based on config.yaml

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 
 import LampToggle from "../LampToggle/LampToggle";
 
@@ -9,14 +10,6 @@ function Header({ config }) {
   const location = useLocation();
   const params = useParams();
   const [isMobile, setIsMobile] = useState(false);
-  if (!config?.site) return null;
-  const { site } = config;
-  const isHome = location.pathname === "/";
-  const isCollection = location.pathname.startsWith("/reader/");
-  
-  const collection = isCollection 
-    ? location.pathname.split('/reader/')[1] 
-    : params.collection;
 
   useEffect(() => {
     function handleResize() {
@@ -27,6 +20,15 @@ function Header({ config }) {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  if (!config?.site) return null;
+  const { site } = config;
+  const isHome = location.pathname === "/";
+  const isCollection = location.pathname.startsWith("/reader/");
+
+  const collection = isCollection 
+    ? location.pathname.split('/reader/')[1] 
+    : params.collection;
+
   let titleClass = "site-title";
   if (isMobile) {
     titleClass += " medium";
@@ -34,35 +36,43 @@ function Header({ config }) {
     titleClass += isHome ? " large" : " small";
   }
 
-  if (isCollection) {
-    return (
-      <header className="header-bar">
-        <div className="header-left collection-view">
-          <Link to="/" className="close-button">
-            {config?.ui?.close || "Close"}
-          </Link>
-          <span className="separator">|</span>
-          <span className="collection-title">{collection}</span>
-        </div>
-        <div className="header-right">
-          <LampToggle />
-        </div>
-      </header>
-    );
-  }
-
   return (
-    <header className="header-bar">
-      <div className="header-left">
-        <Link to="/" className={titleClass}>
-          {site.title}
-        </Link>
-        <p className="site-author">by {site.author}</p>
-      </div>
-      <div className="header-right">
-        <LampToggle />
-      </div>
-    </header>
+    <>
+      <Helmet>
+        <title>{site.title}</title>
+        <meta name="description" content={site.description} />
+        <meta property="og:title" content={site.title} />
+        <meta property="og:description" content={site.description} />
+        <meta property="og:url" content={site.url} />
+        <meta property="og:type" content="website" />
+      </Helmet>
+      {isCollection ? (
+        <header className="header-bar">
+          <div className="header-left collection-view">
+            <Link to="/" className="close-button">
+              {config?.ui?.close || "Close"}
+            </Link>
+            <span className="separator">|</span>
+            <span className="collection-title">{collection}</span>
+          </div>
+          <div className="header-right">
+            <LampToggle />
+          </div>
+        </header>
+      ) : (
+        <header className="header-bar">
+          <div className="header-left">
+            <Link to="/" className={titleClass}>
+              {site.title}
+            </Link>
+            <p className="site-author">by {site.author}</p>
+          </div>
+          <div className="header-right">
+            <LampToggle />
+          </div>
+        </header>
+      )}
+    </>
   );
 }
 
