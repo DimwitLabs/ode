f0c24c1589f6552f6c6d2da70853fc06dde05089
deepanshkhurana <deepanshkhurana@outlook.com>
2025-11-30 19:28:43 +0530

    feat(themes): add recipe, sketch, doodle, and comic presets

---
+++ src/assets/themes/comic.js
@@ -0,0 +1,28 @@
+export default {
+  font: {
+    family: "Lilita One, cursive",
+    url: "https://fonts.googleapis.com/css2?family=Lilita+One&display=swap",
+    fallback: "sans-serif",
+    scale: 1.25
+  },
+  colors: {
+    light: {
+      primary: "#dc2626",
+      secondary: "#ea580c",
+      grey: "#a3a3a3",
+      grey2: "#737373",
+      background: "#fffbeb",
+      text: "#0c0a09",
+      highlight: "#fef3c7"
+    },
+    dark: {
+      primary: "#fca5a5",
+      secondary: "#fdba74",
+      grey: "#525252",
+      grey2: "#404040",
+      background: "#1c1917",
+      text: "#fafaf9",
+      highlight: "#dc262640"
+    }
+  }
+};
